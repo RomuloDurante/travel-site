045c82d3babb9fcfe56f0c84d86492942a45c487
RomuloDurante <romulodurante@hotmail.com>
2018-01-11 16:03:04 -0200

    postcss and pipe

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function(){
   console.log('Gulp Task is ready !');
+});
+
+gulp.task('html', function(){
+  console.log("Your HTML is modifield !");
+});
+
+gulp.task('style', function(){
+  return gulp.src('./app/assets/styles/style.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+/***********************************/
+gulp.task('watch', function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('style');
+  })
 });
