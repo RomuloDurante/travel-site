76a123b602c7276aa65aeb05b0765e94aa797ae7
RomuloDurante <romulodurante@hotmail.com>
2018-01-15 07:52:09 -0200

    gulp cleaner and gulp fixed error

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('cssInject',['style'], function(){
+  return gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
+});
+
+/***********************************/
+
+gulp.task('watch', function(){
+
+  browserSync.init({
+    notify:false,
+    server:{
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function(){
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('cssInject');
+  })
+});
