f83ee4dc00f570a48847bb6e3a8aede28c71a973
RomuloDurante <romulodurante@hotmail.com>
2018-02-07 15:55:39 -0200

    minifier finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -11,7 +11,7 @@ import Modal from './modules/Modal';
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
-new RevealOnScroll($('.feature-item'), '85%');
+new RevealOnScroll($('.feature-item'), '85%'); 
 new RevealOnScroll($('.testimonial'), '60%');
 
 
